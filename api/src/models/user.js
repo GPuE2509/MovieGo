ddd9bfaaf2bfa8deff2afbded3d26dd641fb3cda
GPuE2509 <huygau34@gmail.com>
2025-11-10 07:27:25 +0700

    fix: xóa phần dư trong model

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -18,12 +18,6 @@ const userSchema = new mongoose.Schema({
   point: { type: Number, min: 0, default: 0 },
   roles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Role' }],
   coupons: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Coupon' }],
-  failedLoginAttempts: { type: Number, default: 0 },
-  lastFailedLogin: { type: Date, default: null },
-  resetPasswordToken: { type: String, default: null },
-  resetPasswordExpires: { type: Date, default: null },
-  otp: { type: String, default: null },
-  otpExpires: { type: Date, default: null }
 }, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
 
 // Hash password before saving
